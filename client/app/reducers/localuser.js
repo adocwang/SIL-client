1e63c9d4b4b58fda5c9bdc42cf1bbd2affc038bc
kiefer <xiezhanghuan@ruoshui.me>
2017-01-24 09:16:14 +0800

    add files

---
+++ client/app/reducers/localuser.js
@@ -0,0 +1,16 @@
+/**
+ * Created by kiefer on 2017/1/24.
+ */
+
+'use strict';
+
+import * as types from '../constants/ActionTypes';
+
+const initialState = {
+    data:'init'
+};
+
+export default function localUser (state = initialState, action) {
+    return state
+}
+
