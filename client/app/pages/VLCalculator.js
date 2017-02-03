75f5521cd73672493a7faf9e981dedc51ad57a25
zhangyong <lebron3288@sina.com>
2017-02-03 10:05:15 +0800

    vl计算器

---
+++ client/app/pages/VLCalculator.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+import {TextInput,ScrollView,View,StyleSheet,Text,Image,TouchableWithoutFeedback,Navigator,ListView} from 'react-native'
+import CustomToolbar from '../components/CustomToolbar'
+import CommonColor from '../utils/CommonColor'
+import Button from '../components/Button.ios.js'
+import AcountCertifice from './AcountCertifice.js'
+
+export default class VLCalculator extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+      <CustomToolbar title="现场采集" navigator={this.props.navigator} />
+      <View style={styles.topView}>
+      <Text>融资金额</Text>
+      <TextInput>
+      </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topView: {
+    flexDirection: "row",
+    height: 54,
+    justifyContent:"center",
+    alignItems:"center",
+    borderBottomWidth: 10,
+    borderBottomColor: CommonColor.defaultBgColor,
+  },
+})
